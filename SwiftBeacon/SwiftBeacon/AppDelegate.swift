6343ffbb1e31ce1efb30d2c59c5e9b95b679f973
Gabe Fernandez <gabecfernandez@gmail.com>
2014-07-04 18:51:16 -0500

    Completed iBeacon Swfit implementation - first run
    Still needs testing

--- SwiftBeacon/SwiftBeacon/AppDelegate.swift
+++ SwiftBeacon/SwiftBeacon/AppDelegate.swift
@@ -7,15 +7,47 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
                             
     var window: UIWindow?
+    var locationManager: CLLocationManager?
+    var lastProximity: CLProximity?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         // Override point for customization after application launch.
+        
+        let uuidString = "EBEFD083-70A2-47C8-9837-E7B5634DF524"
+        let beaconIdentifier = "iBeaconModules.us"
+        let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)
+        let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+        
+        // request "Always" authorization for location information (new in iOS 8)
+        locationManager = CLLocationManager()
+        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+        
+        // for iOS 8, must request permission to send notifications
+        // NOTE: for prod app, need to handle the case where use declines
+        if(application.respondsToSelector("registerUserNotificationSettings:")) {
+            application.registerUserNotificationSettings(
+                UIUserNotificationSettings(
+                    forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound,
+                    categories: nil
+                )
+            )
+        }
+        
         return true
     }
 
@@ -42,5 +74,67 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func sendLocalNotificationWithMessage(message: String!) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didRangeBeacons beacons: AnyObject[]!,
+        inRegion region: CLBeaconRegion!) {
+            NSLog("didRangeBeacons");
+            
+            let viewController:ViewController = window!.rootViewController as ViewController
+            viewController.beacons = beacons as CLBeacon[]?
+            viewController.tableView.reloadData()
+            
+            var message:String = ""
+            
+            if(beacons.count > 0) {
+                let nearestBeacon:CLBeacon = beacons[0] as CLBeacon
+                
+                if(nearestBeacon.proximity == lastProximity ||
+                    nearestBeacon.proximity == CLProximity.Unknown) {
+                        return;
+                }
+                lastProximity = nearestBeacon.proximity;
+                
+                switch nearestBeacon.proximity {
+                case CLProximity.Far:
+                    message = "You are far away from the beacon"
+                case CLProximity.Near:
+                    message = "You are near the beacon"
+                case CLProximity.Immediate:
+                    message = "You are in the immediate proximity of the beacon"
+                case CLProximity.Unknown:
+                    return
+                }
+            } else {
+                message = "No beacons are nearby"
+            }
+            
+            NSLog("%@", message)
+            sendLocalNotificationWithMessage(message)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        manager.startRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.startUpdatingLocation()
+        
+        NSLog("You entered the region")
+        sendLocalNotificationWithMessage("You entered the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        manager.stopRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.stopUpdatingLocation()
+        
+        NSLog("You exited the region")
+        sendLocalNotificationWithMessage("You exited the region")
+    }
 }
 
--- SwiftBeacon/SwiftBeacon/ViewController.swift
+++ SwiftBeacon/SwiftBeacon/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var beacons: CLBeacon[]?
+    
+    @IBOutlet var tableView: UITableView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        if(beacons != nil) {
+            return beacons!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell:UITableViewCell? =
+        tableView.dequeueReusableCellWithIdentifier("MyIdentifier") as? UITableViewCell
+        
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle,
+                reuseIdentifier: "MyIdentifier")
+            cell!.selectionStyle = UITableViewCellSelectionStyle.None
+        }
+        
+        let beacon:CLBeacon = beacons![indexPath.row]
+        var proximityLabel:String! = ""
+        
+        switch beacon.proximity {
+        case CLProximity.Far:
+            proximityLabel = "Far"
+        case CLProximity.Near:
+            proximityLabel = "Near"
+        case CLProximity.Immediate:
+            proximityLabel = "Immediate"
+        case CLProximity.Unknown:
+            proximityLabel = "Unknown"
+        }
+        
+        cell!.textLabel.text = proximityLabel
+        
+        let detailLabel:String = "Major: \(beacon.major.integerValue), " +
+            "Minor: \(beacon.minor.integerValue), " +
+            "RSSI: \(beacon.rssi as Int), " +
+        "UUID: \(beacon.proximityUUID.UUIDString)"
+        cell!.detailTextLabel.text = detailLabel
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
